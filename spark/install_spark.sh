078e408f3cd480c8bf86e96efbe6e9d103d8457a
Bernard Louis Alecu <bernardlouisalecu@eduroam-int-dhcp-97-225-180.ucl.ac.uk>
2022-02-11 16:27:31 +0000

    progress

--- spark/install_spark.sh
+++ spark/install_spark.sh
@@ -1,5 +1,7 @@
 sudo apt-get -y install openjdk-8-jdk-headless
-wget -q https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
+wget -q https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz -P /project
+pushd /project
 tar -xzvf spark-3.2.1-bin-hadoop3.2.tgz
+popd
 pip install -q findspark
 pip install pyspark
